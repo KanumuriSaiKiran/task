d319d97df8aba959e526907c9863264611ac8005
unknown <saikiran.com>
2023-07-20 17:51:32 +0530

    commited

---
+++ file.java
@@ -0,0 +1,44 @@
+public class ClassA
+{
+void meth1()
+{
+System.out.println("Hi ");
+System.out.println("Hello ");
+}
+void meth2()
+{
+System.out.print("Hi ");
+System.out.print("Hello ");
+System.out.println();
+}
+void meth3()
+{
+System.out.print("Java is ");
+System.out.println("Object Oriented Programming Language ");
+System.out.println("Java is Awesome ");
+System.out.print("So ");
+System.out.print("I Love It ");
+}
+public static void main(String[] args);
+{
+classA aobj = new ClassA();
+aobj.meth1();
+System.out.println("------");
+aobj.meth2();
+System.out.println("------");
+aobj.meth3();
+}
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
